29d0670b0e49de5138305caec2d3e4671eb404c2
Hilde <hilderic.bourin@gmail.com>
2024-10-27 09:52:54 +0100

    data collection with azure

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,10 +14,74 @@ resource "azurerm_kubernetes_cluster" "k8s" {
         vm_size    = "Standard_B2s"
     }
 
-  identity {
-    type = "SystemAssigned"
-  }
-  
+    identity {
+      type = "SystemAssigned"
+    }
+
+    oms_agent {
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.workspaceLTC.id
+      msi_auth_for_monitoring_enabled = true
+    }
 }
 
 
+resource "azurerm_log_analytics_workspace" "workspaceLTC" {
+  name                = "workspaceLTC"
+  resource_group_name = azurerm_resource_group.ltcDevOps.name
+  location            = azurerm_resource_group.ltcDevOps.location
+}
+
+resource "azurerm_monitor_data_collection_rule" "dcr" {
+  name                = "MSCI-${azurerm_log_analytics_workspace.workspaceLTC.location}-${azurerm_kubernetes_cluster.k8s.name}"
+  resource_group_name = azurerm_resource_group.ltcDevOps.name
+  location            = azurerm_log_analytics_workspace.workspaceLTC.location
+
+  destinations {
+    log_analytics {
+      workspace_resource_id = azurerm_log_analytics_workspace.workspaceLTC.id
+      name                  = "ciworkspace"
+    }
+  }
+
+  data_flow {
+    streams      = var.streams
+    destinations = ["ciworkspace"]
+  }
+
+  data_flow {
+    streams      = [ "Microsoft-Syslog"]
+    destinations = ["ciworkspace"]
+  }
+
+  data_sources {
+    syslog{
+      streams            = ["Microsoft-Syslog"]
+      facility_names      = var.syslog_facilities
+      log_levels          = var.syslog_levels
+      name               = "sysLogsDataSource"
+    }
+
+    extension {
+      streams            = var.streams
+      extension_name     = "ContainerInsights"
+      extension_json     = jsonencode({
+        "dataCollectionSettings" : {
+            "interval": var.data_collection_interval,
+            "namespaceFilteringMode": var.namespace_filtering_mode_for_data_collection,
+            "namespaces": var.namespaces_for_data_collection
+            "enableContainerLogV2": var.enableContainerLogV2
+        }
+      })
+      name               = "ContainerInsightsExtension"
+    }
+  }
+
+  description = "DCR for Azure Monitor Container Insights"
+}
+
+resource "azurerm_monitor_data_collection_rule_association" "dcra" {
+  name                        = "ContainerInsightsExtension"
+  target_resource_id          = azurerm_kubernetes_cluster.k8s.id
+  data_collection_rule_id     = azurerm_monitor_data_collection_rule.dcr.id
+  description                 = "Association of container insights data collection rule. Deleting this association will break the data collection for this AKS Cluster."
+}
--- terraform/var.tf
+++ terraform/var.tf
@@ -8,3 +8,43 @@ variable "cosmosdb_endpoint" {
   description = "cosmosdb_endpoint"
   type        = string
 }
+
+variable "syslog_levels" {
+  type = list(string)
+  default = ["Debug", "Info", "Notice", "Warning", "Error", "Critical", "Alert", "Emergency"]
+}
+
+variable "syslog_facilities" {
+  type = list(string)
+  default = ["auth", "authpriv", "cron", "daemon", "mark", "kern", "local0", "local1", "local2", "local3", "local4", "local5", "local6", "local7", "lpr", "mail", "news", "syslog", "user", "uucp"]
+}
+
+variable "resource_tag_values" {
+  description = "Resource Tag Values"
+  type = map(string)
+  default = {
+    "<existingOrnew-tag-name1>" = "<existingOrnew-tag-value1>"
+    "<existingOrnew-tag-name2>" = "<existingOrnew-tag-value2>"
+    "<existingOrnew-tag-name3>" = "<existingOrnew-tag-value3>"
+  }
+}
+
+variable "data_collection_interval" {
+  default = "1m"
+}
+
+variable "namespace_filtering_mode_for_data_collection" {
+  default = "Off"
+}
+
+variable "namespaces_for_data_collection" {
+  default = ["kube-system", "gatekeeper-system", "azure-arc"]
+}
+
+variable "enableContainerLogV2" {
+  default = true
+}
+
+variable "streams" {
+ default = ["Microsoft-ContainerLog", "Microsoft-ContainerLogV2", "Microsoft-KubeEvents", "Microsoft-KubePodInventory", "Microsoft-KubeNodeInventory", "Microsoft-KubePVInventory","Microsoft-KubeServices", "Microsoft-KubeMonAgentEvents", "Microsoft-InsightsMetrics", "Microsoft-ContainerInventory",  "Microsoft-ContainerNodeInventory", "Microsoft-Perf"]
+}
